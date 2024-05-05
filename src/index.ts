2d9a69f00194dbcd5c57425cbffebe2a8e99cc0c
nicholasrice <nicholasrice@users.noreply.github.com>
2024-05-04 21:57:13 -0700

    adding tests and finishing implementation

--- src/index.ts
+++ src/index.ts
@@ -1,60 +1,43 @@
-import postcss, * as PostCSS from "postcss";
+import { Rule, Declaration, PluginCreator, list } from "postcss";
 
-const name = "postcss-merge-border-radius";
+const name = "PostCSS-merge-border-radius";
 
 /**
  * Options configuration
  */
 export interface Options {}
 
-const pluginFactory: PostCSS.PluginCreator<Options> = (
-  options: Options = {},
-) => {
+const pluginFactory: PluginCreator<Options> = (options: Options = {}) => {
   return {
     postcssPlugin: name,
     Rule: processRule,
   };
 };
+
 pluginFactory.postcss = true;
 
 export default pluginFactory;
 
-const illogicalTester = /border-(top|bottom)-(left|right)-radius/;
-const logicalTester = /border-(start|end)-(start|end)-radius/;
+const tester = /border-(top|bottom)-(left|right)-radius/;
 
-function processRule(rule: PostCSS.Rule): void {
-  reduce(rule, illogicalTester);
-  reduce(rule, logicalTester);
-}
-
-function reduce(rule: PostCSS.Rule, matcher: RegExp): void {
-  const matches = filterDeclarations(rule, matcher);
-  reduceIfAllSame(matches);
-}
-
-/**
- *
- * @param declarations
- */
-function reduceIfAllSame(declarations: PostCSS.Declaration[]): void {
+function processRule(rule: Rule): void {
+  const matches = filterDeclarations(rule, tester);
   const noDuplicateProperties =
-    new Set(declarations.map((value) => value.prop)).size === 4;
-
+    new Set(matches.map((value) => value.prop)).size === 4;
   if (noDuplicateProperties) {
-    if (declarationValuesAndImportantAreEqual(declarations)) {
-      const radiusRule = declarations[0].clone();
-      radiusRule.prop = "border-radius";
-      radiusRule.value = postcss.list.space(radiusRule.value).join(" / ");
-      declarations[0].before(radiusRule);
-      declarations.forEach((value) => value.remove());
-    }
+    const merge = MergeDeclarations.create(
+      matches as [Declaration, Declaration, Declaration, Declaration],
+      rule,
+    );
+
+    merge.merge();
   }
 }
 
 function filterDeclarations(
-  rule: postcss.Rule,
+  rule: Rule,
   matcher: string | RegExp,
-): postcss.Declaration[] {
+): Declaration[] {
   const result = [];
   rule.walkDecls(matcher, (declaration) => {
     result.push(declaration);
@@ -63,21 +46,147 @@ function filterDeclarations(
 }
 
 /**
- * Tests if declaration values and important flags are equal
+ * Represents the 4 corners of a set of declarations that can be merged
+ *
+ * a------b
+ * |      |
+ * d------c
  */
-function declarationValuesAndImportantAreEqual(
-  declarations: postcss.Declaration[],
-) {
-  // Require 2 declarations
-  if (declarations.length < 1) {
-    return false;
+interface IMergeDeclarations {
+  /**
+   * Merge the declarations
+   */
+  merge(): void;
+}
+
+class MergeDeclarations implements IMergeDeclarations {
+  private static aTest = /^border-(start-start|top-left)-radius$/;
+  private static bTest = /^border-(start-end|top-right)-radius$/;
+  private static cTest = /^border-(end-end|bottom-right)-radius$/;
+  private static dTest = /(^border-end-start|bottom-left)-radius$/;
+
+  /**
+   * All values as an array
+   */
+  private values: Readonly<
+    [a: Declaration, b: Declaration, c: Declaration, d: Declaration]
+  >;
+  private constructor(
+    private readonly a: Declaration,
+    private readonly b: Declaration,
+    private readonly c: Declaration,
+    private readonly d: Declaration,
+    private readonly rule: Rule,
+  ) {
+    this.values = Object.freeze([a, b, c, d]);
   }
 
-  const reference = declarations[0];
+  /**
+   * Returns a IMergeDeclaration if the declarations can be merged,
+   * otherwise null.
+   */
+  public static create(
+    declarations: [Declaration, Declaration, Declaration, Declaration],
+    rule: Rule,
+  ): IMergeDeclarations | null {
+    const a = declarations.find((value) => this.aTest.test(value.prop));
+    const b = declarations.find((value) => this.bTest.test(value.prop));
+    const c = declarations.find((value) => this.cTest.test(value.prop));
+    const d = declarations.find((value) => this.dTest.test(value.prop));
 
-  return declarations.every((value) => {
-    return (
-      value.value === reference.value && value.important === reference.important
-    );
-  });
+    if (a !== null && b !== null && c !== null && d !== null) {
+      return new MergeDeclarations(a, b, c, d, rule);
+    } else {
+      return null;
+    }
+  }
+
+  public merge() {
+    const someValuesImportant = this.someValuesImportant;
+    const allValuesImportant = this.allValuesImportant;
+
+    if (someValuesImportant && !allValuesImportant) {
+      // Don't try to merge if only some of the values are !important
+      return;
+    }
+
+    const aSplit = list.space(this.a.value);
+
+    if (this.allValuesEqual) {
+      const value = this.mergeValues([aSplit]);
+      const important = this.a.important;
+      this.mergeRules(value, important);
+      return;
+    }
+
+    const aIsC = this.areEqual(this.a, this.c);
+    const bIsD = this.areEqual(this.b, this.d);
+    const bSplit = list.space(this.b.value);
+    const cSplit = list.space(this.c.value);
+    const dSplit = list.space(this.d.value);
+
+    let value: string;
+    if (aIsC && bIsD) {
+      // reduce to A B / a b
+      value = this.mergeValues([aSplit, bSplit]);
+    } else if (bIsD) {
+      // reduce to A B C / a b c
+      value = this.mergeValues([aSplit, bSplit, cSplit]);
+    } else {
+      // reduce to A B C D / a b c d
+      value = this.mergeValues([aSplit, bSplit, cSplit, dSplit]);
+    }
+
+    this.mergeRules(value, allValuesImportant);
+  }
+
+  private mergeRules(value: string, important?: boolean) {
+    const declaration = new Declaration({
+      prop: "border-radius",
+      value,
+      important,
+    });
+    this.rule.insertBefore(this.a, declaration);
+    this.values.forEach((value) => value.remove());
+  }
+
+  private mergeValues(values: Array<string[]>) {
+    let useSecondPart = false;
+    const firstPart = values.map((values) => values[0]).join(" ");
+    const secondPart = values
+      .map((value) => {
+        if (value[1] !== undefined) {
+          useSecondPart = true;
+        }
+        return value[1] || value[0];
+      })
+      .join(" ");
+
+    return useSecondPart ? `${firstPart} / ${secondPart}` : firstPart;
+  }
+
+  /**
+   * If all the declaration values are equal
+   */
+  private get allValuesEqual() {
+    return this.values.every((value) => this.areEqual(value, this.a));
+  }
+
+  /**
+   * If all the declaration values are important
+   */
+  private get allValuesImportant() {
+    return this.values.every((value) => !!value.important);
+  }
+
+  private get someValuesImportant() {
+    return this.values.some((value) => !!value.important);
+  }
+
+  /**
+   * Test if two declarations are equal to each-other
+   */
+  private areEqual(a: Declaration, b: Declaration) {
+    return a.value === b.value && a.important === b.important;
+  }
 }
--- src/tests/index.spec.ts
+++ src/tests/index.spec.ts
@@ -5,7 +5,8 @@ import Assert from "uvu/assert";
 import prettier from "prettier";
 
 /**
- * Simple tagged-template literal for syntax highlighting
+ * Simple tagged-template literal for syntax highlighting,
+ * and normalizes CSS strings with prettier
  */
 export function css(
   value: TemplateStringsArray,
@@ -36,10 +37,10 @@ test("should merge when all corner values are identical", async () => {
 test("should merge when all corner values are identical variables", async () => {
   const fixture = await css`
     .class {
-      border-top-left-radius: var(-test);
-      border-top-right-radius: var(-test);
-      border-bottom-right-radius: var(-test);
-      border-bottom-left-radius: var(-test);
+      border-top-left-radius: var(--test);
+      border-top-right-radius: var(--test);
+      border-bottom-right-radius: var(--test);
+      border-bottom-left-radius: var(--test);
     }
   `;
 
@@ -71,6 +72,7 @@ test("should merge when all corner values are identical and divided", async () =
   const processed = postcss([plugin]).process(fixture);
   Assert.is(processed.css, result);
 });
+
 test("should not merge when all corner values are identical but !important is not", async () => {
   const fixture = await css`
     .class {
@@ -87,6 +89,46 @@ test("should not merge when all corner values are identical but !important is no
       border-top-right-radius: 10px !important;
       border-bottom-right-radius: 10px;
       border-bottom-left-radius: 10px;
+    }
+  `;
+
+  const processed = postcss([plugin]).process(fixture);
+  Assert.is(processed.css, result);
+});
+
+test("should merge when top-left === bottom-right and top-right === bottom-left", async () => {
+  const fixture = await css`
+    .class {
+      border-top-left-radius: 10px;
+      border-top-right-radius: 20px;
+      border-bottom-right-radius: 10px;
+      border-bottom-left-radius: 20px;
+    }
+  `;
+
+  const result = await css`
+    .class {
+      border-radius: 10px 20px;
+    }
+  `;
+
+  const processed = postcss([plugin]).process(fixture);
+  Assert.is(processed.css, result);
+});
+
+test("should merge when top-right === bottom-left", async () => {
+  const fixture = await css`
+    .class {
+      border-top-left-radius: 10px;
+      border-top-right-radius: 20px;
+      border-bottom-right-radius: 30px;
+      border-bottom-left-radius: 20px;
+    }
+  `;
+
+  const result = await css`
+    .class {
+      border-radius: 10px 20px 30px;
     }
   `;
 
